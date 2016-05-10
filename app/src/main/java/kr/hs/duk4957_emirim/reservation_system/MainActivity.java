3b657d753ebee151886da7735197bb1dcb9571ad
Narae Lee <duk4957@naver.com>
2016-05-10 20:58:20 +0900

    #4 First Event Programming

--- app/src/main/java/kr/hs/duk4957_emirim/reservation_system/MainActivity.java
+++ app/src/main/java/kr/hs/duk4957_emirim/reservation_system/MainActivity.java
@@ -1,5 +1,7 @@
 package kr.hs.duk4957_emirim.reservation_system;
 
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.sql.Time;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     Chronometer chrono;
@@ -50,6 +53,31 @@ public class MainActivity extends AppCompatActivity {
                 calView.setVisibility(View.INVISIBLE);
                 timePick.setVisibility(View.VISIBLE);
 
+            }
+        });
+        butStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+
+            }
+        });
+        butDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+                Calendar cal = Calendar.getInstance();
+                cal.setTimeInMillis(calView.getDate());
+                int year=cal.get(Calendar.YEAR);
+                int month=cal.get(Calendar.MONTH)+1;
+                int date= cal.get(Calendar.DATE);
+                /*int hour=timePick.getHour();
+                int minute=timePick.getMinute();*/
+                String dateAndTime=year+"년"+month+"월"+date+"일";
+                textResult.setText(dateAndTime);
             }
         });
     }
